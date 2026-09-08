5260aca022e4ca45fa70a26875751d57ecb88b62
b17111326-hue <b17111326@gmail.com>
2026-09-07 23:23:00 -0300

    Update validate.js

--- api/validate.js
+++ api/validate.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
   }
 
   const { data, error } = await supabase
-    .from("keys")
+    .from("key")
     .select("id")
     .eq("key", key)
     .maybeSingle();
